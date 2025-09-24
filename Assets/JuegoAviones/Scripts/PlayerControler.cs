d5004bef2cf53fb0de39de431462e5e0170dd050
MikaelaAbrilDz <mikaelaabrildiazvlc@gmail.com>
2025-09-24 19:43:18 +0200

    turbo funcional

--- Assets/JuegoAviones/Scripts/PlayerControler.cs
+++ Assets/JuegoAviones/Scripts/PlayerControler.cs
@@ -1,10 +1,13 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
+using Unity.Cinemachine;
 using UnityEngine.InputSystem.Controls;
 using System.Collections;
 
 public class PlayerControler : MonoBehaviour
 {
+    [SerializeField] CinemachineCamera speedCam;
+
     Vector2 rotation;
     float inclination = 0;
     float speed = 15f;
@@ -12,35 +15,41 @@ public class PlayerControler : MonoBehaviour
 
     private void Start()
     {
-        StartCoroutine(AdjustInclination());
+
     }
     void Update()
     {
-        
-
+        Movement();
+        print(speed);
+    }
+    private void Movement()
+    {        
         transform.position += transform.forward * Time.deltaTime * speed;
         transform.eulerAngles = new Vector3(transform.eulerAngles.x + rotation.y * Time.deltaTime * 50, transform.eulerAngles.y + rotation.x * Time.deltaTime * 50,
             inclination);
-    }
-    private IEnumerator AdjustInclination()
-    {
-        while (true)
-        {
-            yield return new WaitForEndOfFrame();
-            counter += Time.deltaTime * 0.01f / (Mathf.Abs(rotation.x) + 5f);
-            inclination = Mathf.Lerp(inclination, -rotation.x * 15, counter);
 
-            if (inclination == rotation.x) counter = 0;
-        }
+        counter += Time.deltaTime * 0.005f / (Mathf.Abs(rotation.x) + 5f);
+        inclination = Mathf.Lerp(inclination, -rotation.x * 15, counter);
+
+        if (inclination == rotation.x) counter = 0;
     }
     private void OnMove(InputValue movementValue)
     {
         rotation.x = movementValue.Get<Vector2>().x;
         rotation.y = movementValue.Get<Vector2>().y;
     }
-    private void OnTurbo()
+    private void OnTurbo(InputValue turbo)
     {
-        if (true) speed = 30f;
-        else speed = 15f;
+        if (turbo.isPressed)
+        {
+            speed = 30f;
+            speedCam.Priority = 1;
+        }
+        else
+        {
+            speed = 15f;
+            speedCam.Priority = -1;
+            
+        }
     }
 }
