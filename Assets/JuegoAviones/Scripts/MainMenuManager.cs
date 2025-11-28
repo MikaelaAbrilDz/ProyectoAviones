a900100ada4de897e30ccc908c9b1671508d55b9
Mikaela <mikaelaabrildiazvlc@gmail.com>
2025-11-28 14:45:45 +0100

    implementación del relay

--- Assets/JuegoAviones/Scripts/MainMenuManager.cs
+++ Assets/JuegoAviones/Scripts/MainMenuManager.cs
@@ -1,11 +1,31 @@
+using System.Threading.Tasks;
 using Unity.Netcode;
+using Unity.Netcode.Transports.UTP;
+using Unity.Services.Authentication;
+using Unity.Services.Core;
+using Unity.Services.Relay;
+using Unity.Services.Relay.Models;
 using UnityEngine;
 
 public class MainMenuManager : MonoBehaviour
 {
-    public void Host()
+    public async Task<string> StartHostWithRelay(int maxConnections, string connectionType)
     {
-        NetworkManager.Singleton.StartHost();
+        await UnityServices.InitializeAsync();
+        if (!AuthenticationService.Instance.IsSignedIn)
+        {
+            await AuthenticationService.Instance.SignInAnonymouslyAsync();
+        }
+        var allocation = await RelayService.Instance.CreateAllocationAsync(maxConnections);
+        NetworkManager.Singleton.GetComponent<UnityTransport>().SetRelayServerData(AllocationUtils.ToRelayServerData(allocation, connectionType));
+        var joinCode = await RelayService.Instance.GetJoinCodeAsync(allocation.AllocationId);
+        EndSelection();
+        return NetworkManager.Singleton.StartHost() ? joinCode : null;
+    }
+
+    public async void Host()
+    {
+        print (await StartHostWithRelay(2, "udp"));
         EndSelection();
     }
     public void Client()
