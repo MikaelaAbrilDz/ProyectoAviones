d35f451a12fee47171c428acba9f5f233243edb3
MikaelaAbrilDz <mikaelaabrildiazvlc@gmail.com>
2025-12-15 14:05:51 +0100

    Relay implementado (ya funciona en dos ordenadores distintos)

--- Assets/JuegoAviones/Scripts/HostClientMenuManager.cs
+++ Assets/JuegoAviones/Scripts/HostClientMenuManager.cs
@@ -7,9 +7,26 @@ using Unity.Services.Relay;
 using Unity.Services.Relay.Models;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using TMPro;
+using UnityEngine.UI;
 
 public class HostClientMenuManager : MonoBehaviour
 {
+    [SerializeField] GameObject hostPanel, clientPanel;
+    [SerializeField] TextMeshProUGUI generatedCode;
+    [SerializeField] TMP_InputField enteredCode;
+    private void Update()
+    {
+        if (NetworkManager.Singleton.ConnectedClientsList.Count == 2) EndSelection();        
+    }
+    public void OpenHostOptions()
+    {
+        hostPanel.SetActive(true);
+    }
+    public void OpenClientOptions()
+    {
+        clientPanel.SetActive(true);
+    }
     public async Task<string> StartHostWithRelay(int maxConnections, string connectionType)
     {
         await UnityServices.InitializeAsync();
@@ -20,23 +37,33 @@ public class HostClientMenuManager : MonoBehaviour
         var allocation = await RelayService.Instance.CreateAllocationAsync(maxConnections);
         NetworkManager.Singleton.GetComponent<UnityTransport>().SetRelayServerData(AllocationUtils.ToRelayServerData(allocation, connectionType));
         var joinCode = await RelayService.Instance.GetJoinCodeAsync(allocation.AllocationId);
-        EndSelection();
         return NetworkManager.Singleton.StartHost() ? joinCode : null;
     }
+    public async Task<bool> StartClientWithRelay(string joinCode, string connectionType)
+    {
+        await UnityServices.InitializeAsync();
+        if (!AuthenticationService.Instance.IsSignedIn)
+        {
+            await AuthenticationService.Instance.SignInAnonymouslyAsync();
+        }
 
-    public void Host()
-    {
-        //print (await StartHostWithRelay(2, "udp"));
-        NetworkManager.Singleton.StartHost();
-        EndSelection();
+        var allocation = await RelayService.Instance.JoinAllocationAsync(joinCode: joinCode);
+        NetworkManager.Singleton.GetComponent<UnityTransport>().SetRelayServerData(AllocationUtils.ToRelayServerData(allocation, connectionType));
+        return !string.IsNullOrEmpty(joinCode) && NetworkManager.Singleton.StartClient();
     }
-    public void Client()
+    public async void Host()
     {
-        NetworkManager.Singleton.StartClient();
-        EndSelection();
+        Time.timeScale = 0;
+        generatedCode.text = await StartHostWithRelay(2, "udp");
+    }
+    public async void Client()
+    {
+        Time.timeScale = 0;
+        await StartClientWithRelay(enteredCode.text, "udp");
     }
     private void EndSelection()
     {
+        Time.timeScale = 1;
         gameObject.SetActive(false);
     }
 }
